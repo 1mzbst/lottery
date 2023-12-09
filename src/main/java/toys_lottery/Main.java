76bd93963504de4c395d8d356535db8f26a73926
unknown <dk4git@outlook.com>
2023-12-09 13:50:35 +0300

    have added two toys in the Main

--- src/main/java/toys_lottery/Main.java
+++ src/main/java/toys_lottery/Main.java
@@ -1,7 +1,11 @@
 package toys_lottery;
 
+import toys_lottery.toys.Toys;
+
 public class Main {
     public static void main(String[] args) {
 
+        Toys firstToy = new Toys(1, "ПатПат", 1);
+        Toys secondToy = new Toys(2, "Кря-Кря", 4);
     }
 }
