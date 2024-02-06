3e5e9a73daea5cdd20dd22be72d1ded093d9754c
unknown <dk4git@outlook.com>
2024-02-06 14:21:25 +0300

    moving on

--- src/main/java/toys_lottery/player/GetPlayer.java
+++ src/main/java/toys_lottery/player/GetPlayer.java
@@ -8,8 +8,11 @@ public class GetPlayer {
         System.out.println("Выберите количество участников");
         int number_from_scanner = scanner.nextInt();
         int test2 = scanner.nextInt();
-        Player exmpl = new Player("qsf", number_from_scanner);
+//        Player exmpl = new Player("qsf", number_from_scanner);
+        System.out.println("Напишите Фамилию Имя Отчество и ваш возраст.");
         for (int i = 0; i <=number_from_scanner; i++) {
+            System.out.println("Участник под номером" + (i + 1) + ":");
+
             // Дописать запись игрока
         }
 return exmpl;
